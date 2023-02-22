eacd347c30b7172bf143a63bed3dad2d6ca142c2
SiobhanS16 <113051881+SiobhanS16@users.noreply.github.com>
2023-02-22 14:02:37 -0800

    deleted login and made aboutus page work

--- app/routes/default.py
+++ app/routes/default.py
@@ -6,10 +6,10 @@ from flask import render_template
 def aboutus():
     return render_template('aboutus.html')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/')
-def homepage():
+def index():
     return render_template('homepage.html')
