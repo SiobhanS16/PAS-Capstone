2a03936a51a55e8e23c59ecd6856300da17db259
SiobhanS16 <113051881+SiobhanS16@users.noreply.github.com>
2023-04-14 09:32:12 -0700

    changes survey.py to match to the different ones

--- app/routes/survey.py
+++ app/routes/survey.py
@@ -7,7 +7,7 @@ import mongoengine.errors
 import random
 from flask import render_template, flash, redirect, url_for
 from app.classes.data import Survey
-from app.classes.forms import SurveyForm
+from app.classes.forms import SurveyForm1, SurveyForm2, SurveyForm3
 import datetime as dt
 
 # This route actually does two things depending on the state of the if statement 
@@ -22,9 +22,91 @@ import datetime as dt
 # This means the user must be logged in to see this page
 # @login_required
 # This is a function that is run when the user requests this route.
-def surveyNew():
+def surveyNew1():
     # This gets the form object from the form.py classes that can be displayed on the template.
-    form = SurveyForm()
+    form = SurveyForm1()
+
+    # This is a conditional that evaluates to 'True' if the user submitted the form successfully.
+    # validate_on_submit() is a method of the form object. 
+    if form.validate_on_submit():
+
+        # This stores all the values that the user entered into the new blog form. 
+        # Blog() is a mongoengine method for creating a new blog. 'newBlog' is the variable 
+        # that stores the object that is the result of the Blog() method.  
+        newSurvey = Survey(
+            # the left side is the name of the field from the data table
+            # the right side is the data the user entered which is held in the form object.
+            subject = form.subject.data,
+            content = form.content.data,
+            tag = form.tag.data,
+            # This sets the modifydate to the current datetime.
+            modify_date = dt.datetime.utcnow
+        )
+        newSurvey.save()
+        newSurvey.reload()
+        if form.audio.data:
+            newSurvey.audio.put(form.audio.data, content_type = 'audio/mpeg')
+        # This is a method that saves the data to the mongoDB database.
+        newSurvey.save()
+
+        # Once the new blog is saved, this sends the user to that blog using redirect.
+        # and url_for. Redirect is used to redirect a user to different route so that 
+        # routes code can be run. In this case the user just created a blog so we want 
+        # to send them to that blog. url_for takes as its argument the function name
+        # for that route (the part after the def key word). You also need to send any
+        # other values that are needed by the route you are redirecting to.
+        return redirect(url_for('survey',surveyID=newSurvey.id))
+
+    # if form.validate_on_submit() is false then the user either has not yet filled out
+    # the form or the form had an error and the user is sent to a blank form. Form errors are 
+    # stored in the form object and are displayed on the form. take a look at blogform.html to 
+    # see how that works.
+    return render_template('surveyform.html',form=form)
+
+def surveyNew2():
+    # This gets the form object from the form.py classes that can be displayed on the template.
+    form = SurveyForm2()
+
+    # This is a conditional that evaluates to 'True' if the user submitted the form successfully.
+    # validate_on_submit() is a method of the form object. 
+    if form.validate_on_submit():
+
+        # This stores all the values that the user entered into the new blog form. 
+        # Blog() is a mongoengine method for creating a new blog. 'newBlog' is the variable 
+        # that stores the object that is the result of the Blog() method.  
+        newSurvey = Survey(
+            # the left side is the name of the field from the data table
+            # the right side is the data the user entered which is held in the form object.
+            subject = form.subject.data,
+            content = form.content.data,
+            tag = form.tag.data,
+            # This sets the modifydate to the current datetime.
+            modify_date = dt.datetime.utcnow
+        )
+        newSurvey.save()
+        newSurvey.reload()
+        if form.audio.data:
+            newSurvey.audio.put(form.audio.data, content_type = 'audio/mpeg')
+        # This is a method that saves the data to the mongoDB database.
+        newSurvey.save()
+
+        # Once the new blog is saved, this sends the user to that blog using redirect.
+        # and url_for. Redirect is used to redirect a user to different route so that 
+        # routes code can be run. In this case the user just created a blog so we want 
+        # to send them to that blog. url_for takes as its argument the function name
+        # for that route (the part after the def key word). You also need to send any
+        # other values that are needed by the route you are redirecting to.
+        return redirect(url_for('survey',surveyID=newSurvey.id))
+
+    # if form.validate_on_submit() is false then the user either has not yet filled out
+    # the form or the form had an error and the user is sent to a blank form. Form errors are 
+    # stored in the form object and are displayed on the form. take a look at blogform.html to 
+    # see how that works.
+    return render_template('surveyform.html',form=form)
+
+def surveyNew3():
+    # This gets the form object from the form.py classes that can be displayed on the template.
+    form = SurveyForm3()
 
     # This is a conditional that evaluates to 'True' if the user submitted the form successfully.
     # validate_on_submit() is a method of the form object. 
